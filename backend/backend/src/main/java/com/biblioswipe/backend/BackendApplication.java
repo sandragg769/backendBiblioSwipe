0cd84d54b0685c5e10649f16600c04f86944bf85
Sandra <sandragg67@gmail.com>
2025-10-27 09:49:34 +0100

    FUNCIONA al ejecutar crea las tablas, explicacion en backendApplication

--- backend/backend/src/main/java/com/biblioswipe/backend/BackendApplication.java
+++ backend/backend/src/main/java/com/biblioswipe/backend/BackendApplication.java
@@ -3,11 +3,13 @@ package com.biblioswipe.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//al ejecutarlo con el run se activa el servidor embebido se spring boot (pone a mi backend a
+// escuchar peticiones HTTP) por defecto el puerto 8080, con esto ya podemos usar postman o navegador o frontend
 @SpringBootApplication
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
 
 }

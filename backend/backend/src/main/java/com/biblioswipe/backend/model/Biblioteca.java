95c8d61c63d7241284430007ac5c2dc0d6fc2ffc
Sandra Gómez García <sandragm67@gmail.com>
2025-11-19 18:28:54 +0100

    pruebas usu terminadas

--- backend/backend/src/main/java/com/biblioswipe/backend/model/Biblioteca.java
+++ backend/backend/src/main/java/com/biblioswipe/backend/model/Biblioteca.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
